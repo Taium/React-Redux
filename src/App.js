97145f94780af4749c4325a553d759690cfb3536
Taium <taium44@gmail.com>
2021-03-09 20:48:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import {Provider} from 'react-redux'
 import './App.css';
+import store from './components/redux/store';
+import Cackecont from './Cackecont';
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>tttttttaauiunmm</h1>
+      <Cackecont/>
     </div>
+    </Provider>
   );
 }
 
---
+++ src/components/redux/store.js
@@ -0,0 +1,8 @@
+import cakeReducer from "./cake/cakeReducer";
+import {createStore} from "redux"
+
+
+
+const store = createStore(cakeReducer);
+
+export default store;
